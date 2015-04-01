850d2970d42241d656b3f1a42a0798165ceca6f8
Yves Serrano <y@yas.ch>
2015-04-01 15:38:19 +0200

    create host keys if needed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,9 @@ if [ "$1" = 'sshserver' ]; then
   # SETUP
   mkdir -p /root/.ssh /var/run/sshd
   cp /root/id.pub /root/.ssh/authorized_keys
+  if [ ! -f "/etc/ssh/ssh_host_rsa_key" ]; then
+    ssh-keygen -A
+  fi
 
   # RUN sshd
   echo "* started sshd"
