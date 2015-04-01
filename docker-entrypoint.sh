2a491448319fd2f21e00f15aa10225c88465cd6a
Yves Serrano <y@yas.ch>
2015-04-01 15:38:41 +0200

    add id_rsa.pub or id_dsa_pub to authorized keys

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,9 +4,14 @@ set -e
 if [ "$1" = 'sshserver' ]; then
   # SETUP
   mkdir -p /root/.ssh /var/run/sshd
-  cp /root/id.pub /root/.ssh/authorized_keys
   if [ ! -f "/etc/ssh/ssh_host_rsa_key" ]; then
     ssh-keygen -A
+  fi
+  if [ -f "/root/id_rsa.pub" ]; then
+    cp /root/id_rsa.pub /root/.ssh/authorized_keys
+  fi
+  if [ -f "/root/id_dsa.pub" ]; then
+    cp /root/id_dsa.pub /root/.ssh/authorized_keys
   fi
 
   # RUN sshd
